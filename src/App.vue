bdeb6027fd3373caf5c03476efe5759eed774570
Kent Taylor <ketaylor@gmail.com>
2021-07-07 08:31:05 -0500

    new pic

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/gone-phisin-brutalwisconsin.png">
+  <HelloWorld msg="Welcome to Your New Hell.js"/>
 </template>
 
 <script>
