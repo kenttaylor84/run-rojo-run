f5501576b631412b6ee07a60f0281c27a1a668db
Kent Taylor <ketaylor@gmail.com>
2021-07-07 17:08:43 -0500

    flag background

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
+<video autoplay muted loop id="myVideo">
+  <source src="./assets/ru_flag.mp4" type="video/mp4">
+</video>
+<div id="content">
   <img alt="Vue logo" src="./assets/gone-phisin-brutalwisconsin.png">
   <HelloWorld msg="Welcome to Your New Hell.js"/>
+</div>
 </template>
 
 <script>
@@ -22,5 +27,19 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#myVideo {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  min-width: 100%;
+  min-height: 100%;
+}
+#content {
+  position: fixed;
+  bottom: 0;
+  color: #f1f1f1;
+  width: 100%;
+  padding: 20px;
 }
 </style>
